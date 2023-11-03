e943eaad0c4f7d6a9ebd1f5d12876dd534a70ab3
alex_accelerator <alex.acelerator@gmail.com>
2023-11-03 14:33:35 +0300

    Third commit

--- app/src/main/java/com/alex_acelerator/bitaddress/SimpleGenerationPage.kt
+++ app/src/main/java/com/alex_acelerator/bitaddress/SimpleGenerationPage.kt
@@ -1,11 +1,19 @@
 package com.alex_acelerator.bitaddress
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Checkbox
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,26 +21,55 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun SimpleGenerationPage(
     navController: NavController
 ) {
     Column {
+        Spacer(modifier = Modifier.height(24.dp))
         Text(
-            text = "New Address",
-            modifier = Modifier.fillMaxWidth()
+            text = stringResource(id = R.string.Address),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(32.dp)
+                .padding(8.dp)
+                .background(color = Color.Gray)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = stringResource(id = R.string.PublicKey),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(32.dp)
+                .padding(8.dp)
+                .background(color = Color.Gray)
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        var temp by remember {mutableStateOf(false)}
-        Checkbox(checked = temp, onCheckedChange = {value -> temp = value})
+        Row(modifier = Modifier.fillMaxWidth()) {
+            var privateKey by remember { mutableStateOf("Some PrivateKey") }
+            OutlinedTextField(value = privateKey, onValueChange = { privateKey = it },
+
+            )
+            Icon(
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .size(56.dp),
+                imageVector = Icons.Default.Refresh,
+                contentDescription = null
+                )
+        }
 
     }
 }
