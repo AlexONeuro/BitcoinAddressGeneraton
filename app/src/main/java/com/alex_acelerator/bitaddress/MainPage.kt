cf9aaa33f82cd4fbf3048e14f94a48704fdf9fde
alex_accelerator <alex.acelerator@gmail.com>
2023-11-03 14:33:35 +0300

    Third commit
    Signed-off-by: alex_accelerator <alex.acelerator@gmail.com>

--- app/src/main/java/com/alex_acelerator/bitaddress/MainPage.kt
+++ app/src/main/java/com/alex_acelerator/bitaddress/MainPage.kt
@@ -2,6 +2,7 @@ package com.alex_acelerator.bitaddress
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,12 +16,21 @@ import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,105 +42,48 @@ fun MainPage(
     navController: NavController
 ) {
 
-    Column{
+    Column {
 
         Spacer(modifier = Modifier.height(48.dp))
-
-        Card(
-            colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.surfaceVariant
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable(onClick = { navController.navigate("SimpleGenerationPage") })
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Icon(
-                    modifier = Modifier
-                        .size(120.dp)
-                        .background(color = Color.Gray),
-
-                    imageVector = Icons.Default.Lock,
-                    contentDescription = "Sample1"
+        Box() {
+            CardElement(
+                navController = navController,
+                route = stringResource(id = R.string.SimpleGenerationPage),
+                imageVector = Icons.Default.Lock,
+                title = "Generation",
+                explainingText = "Use this to generate new address"
+            )
+            var checkedState by remember { mutableStateOf(false) }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(modifier = Modifier.align(Alignment.BottomStart)) {
+                Checkbox(
+                    checked = checkedState, onCheckedChange = { checkedState = it },
+                    modifier = Modifier.size(48.dp)
                 )
-                Column() {
-                    Text(
-                        text = "GENERATION",
-                        modifier = Modifier
-                            .padding(16.dp),
-                        fontSize = 32.sp
-                    )
-                    Text(
-                        text = "Use this option to generate new address",
-                        modifier = Modifier
-                            .padding(16.dp),
-                        fontSize = 12.sp
-                    )
-                }
-
+                Text(text = "Advansed address creation", modifier = Modifier.align(CenterVertically))
             }
-            Spacer(modifier = Modifier.height(24.dp))
+
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(24.dp))
 
-        Card(
-            colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.surfaceVariant
-            ),
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Icon(
-                    modifier = Modifier
-                        .size(120.dp)
-                        .background(color = Color.Gray),
+        CardElement(
+            navController = navController,
+            route = "",
+            imageVector = Icons.Default.Settings,
+            title = "Convertation",
+            explainingText = "Use this to convert private address to list of words and vise versa"
+        )
 
-                    imageVector = Icons.Default.Settings,
-                    contentDescription = "Sample1"
-                )
-                Text(
-                    text = "СONVERTATION",
-                    modifier = Modifier
-                        .padding(16.dp),
-                    fontSize = 32.sp
-                )
-            }
-            Spacer(modifier = Modifier.height(24.dp))
-        }
+        Spacer(modifier = Modifier.height(24.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Card(
-            colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.surfaceVariant
-            ),
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Icon(
-                    modifier = Modifier
-                        .size(120.dp)
-                        .background(color = Color.Gray),
-
-                    imageVector = Icons.Default.Phone,
-                    contentDescription = "Sample1"
-                )
-                Text(
-                    text = "СONNECTION",
-                    modifier = Modifier
-                        .padding(16.dp),
-                    fontSize = 32.sp
-                )
-            }
-            Spacer(modifier = Modifier.height(24.dp))
-        }
+        CardElement(
+            navController = navController,
+            route = "",
+            imageVector = Icons.Default.Phone,
+            title = "Connection",
+            explainingText = "Check your balance!"
+        )
     }
 }
 
@@ -139,3 +92,51 @@ fun MainPage(
 fun MainPagePreview() {
     MainPage(navController = rememberNavController())
 }
+
+@Composable
+fun CardElement(
+    navController: NavController,
+    route: String,
+    imageVector: ImageVector,
+    title: String,
+    explainingText: String
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = { navController.navigate(route) })
+            .height(170.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(120.dp)
+                    .background(color = Color.Gray),
+
+                imageVector = imageVector,
+                contentDescription = "Sample1"
+            )
+            Column() {
+                Text(
+                    text = title,
+                    modifier = Modifier
+                        .padding(16.dp),
+                    fontSize = 32.sp
+                )
+                Text(
+                    text = explainingText,
+                    modifier = Modifier
+                        .padding(16.dp),
+                    fontSize = 12.sp
+                )
+            }
+
+        }
+    }
+}
+
